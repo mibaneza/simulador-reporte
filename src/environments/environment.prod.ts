ea083d9fd80df8e51c3daecaf7bf27477d26df41
mibaneza <49458798+mibaneza@users.noreply.github.com>
2023-07-12 00:11:44 -0500

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 
 export const environment = {
   production: true,
-  HOST: 'https://516f-2800-4b0-4005-e927-c155-feb2-3be-fc7e.ngrok-free.app',
+  HOST: 'https://simulador.brossolution.pe',
+  HOST_2: 'https://simulador.brossolution.pe',
 };
