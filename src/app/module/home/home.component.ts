a9eb52350f699c624fc0cb85709ca54eb4920e40
mibaneza <49458798+mibaneza@users.noreply.github.com>
2023-08-07 12:48:31 -0500

    xd

--- src/app/module/home/home.component.ts
+++ src/app/module/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { CarreraModel, FormDataModel, HttpService, InstitucionModel } from 'src/app/service/http.service';
 
 @Component({
@@ -7,10 +7,22 @@ import { CarreraModel, FormDataModel, HttpService, InstitucionModel } from 'src/
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit{
+  screenWidth?: number ;
+  widthImg:number = 300;
   ngOnInit(): void {
    
   }
-
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = window.innerWidth;
+    if(window.innerWidth <= 575 ){
+      this.widthImg = 200;
+    }
+    if(window.innerWidth >= 576   ){
+      this.widthImg = 300;
+    }
+    console.log(this.screenWidth)
+  }
 }
 
  
